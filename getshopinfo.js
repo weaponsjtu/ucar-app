d65ccb14868ff3339b1a19727b7835ac14f9a148
javawebzwp <javawebzwp@gmail.com>
2012-09-02 04:02:39 +0000

    1.修正服务端商户数据读取错误的问题； shopinfo.php 2.增加DEBUG控制变量，以后用这个变量来决定是测试模式还是产品模式； index.html

--- getshopinfo.js
+++ getshopinfo.js
@@ -1,27 +1,38 @@
-function showshop(shop)
+function showshop(shop, type)
 	{
 $(document).ready(function(){
 	var html ='';
 	var tag = '';
 	var star = '';
-	$.getJSON('http://ihavecar.sinaapp.com/shopinfo.php?shop_id='+shop+'&jasoncallback=?&randomID='+Math.random(), function(json){
+	$.getJSON('http://ihavecar.sinaapp.com/shopinfo.php?shop_id='+shop+'&type='+type+'&jasoncallback=?&randomID='+Math.random(), function(json){
 		//random函数用来取消缓存影响,jsonp形式进行跨域传数据
 		$.each(json,function(entryIndex,entry){
-	            html+='<div style="font-size: 18px;"><b>'+entry['业户名称']+'</b></div>'; 
+			if (type == '1') {
+	      html+='<div style="font-size: 18px;"><b>'+entry['业户名称']+'</b></div>'; 
 				html+='<hr size="1" color="#AAA"   align="center" />';
-				html+='<div><a href="tel:13761445702">许可证号：'+entry['许可证号']+'</a></div>'; 
+				html+='<div><a href="tel:62935474">许可证号：'+entry['许可证号']+'</a></div>'; 
 				html+='<hr size="1" color="#AAA"   align="center" />';
-	            html+='<div>经营地址：'+entry['经营地址']+'</div>';   
+	            html+='<div>地址：'+entry['经营地址']+'</div>';   
 				tag += entry['经营范围'];
 				star += '<span id="star"></span><font style="float:right">共有' + entry['评价人数'] + '个评分</font><script type="text/javascript">$(function(){$("#star").raty({readOnly:	true,start: '+entry['评价']+'	});});</script>'; 
-				$('#lng').html(entry['经度']);
-				$('#lat').html(entry['纬度']);
-	        });  
-	    $('#shopinfo').html(html); 
+			} 
+			if (type == '2') {
+				html+='<div style="font-size: 18px;"><b>'+entry['加油站名称']+'</b></div>'; 
+				html+='<hr size="1" color="#AAA"   align="center" />';
+				html+='<div><a href="tel:' + entry['联系电话'] +'">电话：'+entry['联系电话']+'</a></div>'; 
+				html+='<hr size="1" color="#AAA"   align="center" />';
+	            html+='<div>地址：'+entry['加油站地址']+'</div>';   
+				tag += entry['加油站属性'];
+				star += '<span id="star"></span><font style="float:right">共有' + entry['评分人数'] + '个评分</font><script type="text/javascript">$(function(){$("#star").raty({readOnly:	true,start: '+entry['评分总和']+'	});});</script>'; 
+			}
+			$('#lng').html(entry['经度']);
+			$('#lat').html(entry['纬度']);
+	  });  
+	  $('#shopinfo').html(html); 
 		$('#shoptag').html(tag);
 		$('#shopstar').html(star);
-		}); 
-	});  
+	}); 
+});  
 
 
-		}// JavaScript Document
+}// JavaScript Document
