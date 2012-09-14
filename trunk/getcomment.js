73513da5282f94e4d3d165f4528b6cdf80b30835
wangyichao0122@gmail.com <wangyichao0122@gmail.com@75b40f71-ecbc-670a-4c62-a22b064bb89e>
2012-09-14 07:02:19 +0000

    修复评论头像无法显示bug

--- trunk/getcomment.js
+++ trunk/getcomment.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	$.getJSON('http://ihavecar.sinaapp.com/usercomment.php?shop_id='+shop+'&jasoncallback=?&randomID='+Math.random(), function(json){
 		//random函数用来取消缓存影响,jsonp形式进行跨域传数据
 		$.each(json,function(entryIndex,entry){
-				html+='<tr><td><img style="padding: 10px;" src="'+entry['photo']+'" width="60" height="60"></td>';
+				html+='<tr><td><img style="padding: 10px;" src="'+entry['image']+'" width="60" height="60"></td>';
 	            html+='<td><b>'+entry['nickname'] + '</b>&nbsp;&nbsp;&nbsp;&nbsp;'; 
 				html+='<span id="star'+entry['commentid']+'"></span><script type="text/javascript">$(function(){$("#star'+entry['commentid']+'").raty({readOnly:	true,start:		'+entry['score']+'	});});</script>'; 
 	            html+='<br><p>'+entry['content']+'</p>'; 
