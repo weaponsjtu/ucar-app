561f228956f9abf256a9b75df065575eccf93e15
javawebzwp@gmail.com <javawebzwp@gmail.com@75b40f71-ecbc-670a-4c62-a22b064bb89e>
2012-09-16 04:31:28 +0000

    修复bug：<我的收藏>内，“编辑”后，“Delete”按钮覆盖在商户信息上，不占用商户信息介绍文字空间，避免出现文字换行，导致格式被更换

--- trunk/getcollection.js
+++ trunk/getcollection.js
@@ -1,6 +1,6 @@
 function getcollection(uid) {
 	$(document).ready(function(){
-		var html='<table>';	
+		var html='<table id="table">';	
 		$.getJSON('http://ihavecar.sinaapp.com/collection.php?jasoncallback=?&uid='+uid+'&randomID='+Math.random(), function(json){
 		//random函数用来取消缓存影响,jsonp形式进行跨域传数据
 			$.each(json,function(entryIndex,entry){
