e0f8a7df5385150112e355e1235dfb766389a9b5
wangyichao0122@gmail.com <wangyichao0122@gmail.com@75b40f71-ecbc-670a-4c62-a22b064bb89e>
2012-10-05 14:02:17 +0000

    将评论修改为初始状态只显示5条，点击按钮后显示全部

--- trunk/getcomment.js
+++ trunk/getcomment.js
@@ -1,6 +1,30 @@
 function showcomment(shop)
 	{
 $(document).ready(function(){
+	var count=0;
+	var html='<table>';	
+	$.getJSON('http://ihavecar.sinaapp.com/usercomment.php?shop_id='+shop+'&jasoncallback=?&randomID='+Math.random(), function(json){
+		//random函数用来取消缓存影响,jsonp形式进行跨域传数据
+		$.each(json,function(entryIndex,entry){
+			if(count<5){
+				html+='<tr><td><img style="padding: 10px;" src="'+entry['image']+'" width="60" height="60"></td>';
+	            html+='<td><b>'+entry['nickname'] + '</b>&nbsp;&nbsp;&nbsp;&nbsp;'; 
+				html+='<span id="star'+entry['commentid']+'"></span><script type="text/javascript">$(function(){$("#star'+entry['commentid']+'").raty({readOnly:	true,start:		'+entry['score']+'	});});</script>'; 
+	            html+='<br><p>'+entry['content']+'</p>'; 
+				//html+=''+entry['time']+'  </td>';    
+	            html+='</tr>';}
+				count++;
+	    });
+		html+='</table>';
+	    $('#comment').html(html); 
+	}); 
+});  
+}
+		// JavaScript Document
+		
+function showall(shop)
+	{
+$(document).ready(function(){
 	var html='<table>';	
 	$.getJSON('http://ihavecar.sinaapp.com/usercomment.php?shop_id='+shop+'&jasoncallback=?&randomID='+Math.random(), function(json){
 		//random函数用来取消缓存影响,jsonp形式进行跨域传数据
@@ -10,14 +34,10 @@ $(document).ready(function(){
 				html+='<span id="star'+entry['commentid']+'"></span><script type="text/javascript">$(function(){$("#star'+entry['commentid']+'").raty({readOnly:	true,start:		'+entry['score']+'	});});</script>'; 
 	            html+='<br><p>'+entry['content']+'</p>'; 
 				//html+=''+entry['time']+'  </td>';    
-	            html+='</tr>';  
-				
+	            html+='</tr>';
 	    });
-		html+='</table>'   
+		html+='</table>';
 	    $('#comment').html(html); 
 	}); 
 });  
-
-
-		}
-		// JavaScript Document
+}
