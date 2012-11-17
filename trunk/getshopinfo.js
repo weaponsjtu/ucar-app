be93b25a9d799965687e3340129c09835f9572df
javawebzwp@gmail.com <javawebzwp@gmail.com@75b40f71-ecbc-670a-4c62-a22b064bb89e>
2012-11-17 03:40:29 +0000

    修复停车场模块的商户详情页面的bug，通过真机测试。不过这个真机上，一直写文件不能。

--- trunk/getshopinfo.js
+++ trunk/getshopinfo.js
@@ -7,24 +7,16 @@ $(document).ready(function(){
 		//random函数用来取消缓存影响,jsonp形式进行跨域传数据
 		$.each(json,function(entryIndex,entry){
 			
-			shopname=entry['企业名称'];
+				shopname=entry['企业名称'];
 	      html+='<div style="font-size: 24px; "><b>'+entry['企业名称']+'</b></div><br>'; 
 				html+='<div id="shopdetails"><div><br><a href="tel:' + entry['联系电话'] + '" style="font-size: 18px; padding: 10px 0 0 10px;">电话：'+entry['联系电话']+'</a></div>'; 
 				html+='<hr size="1" color="#AAA"   align="center" />';
 	      html+='<div style="font-size: 18px; padding: 0 0 10px 10px;">地址：'+entry['企业地址']+'</div></div>';   
 				star += '<span id="star"></span><font style="float:right">共有' + entry['评分人数'] + '个评分</font><script type="text/javascript">$(function(){$("#star").raty({readOnly:	true,size:      24,'
-		+ 'starHalf:  "star-half-big.png",'
-		+ 'starOff:   "star-off-big.png",'
-		+ 'starOn:    "star-on-big.png", start: '+entry['评分']+'	});});</script>'; 
-			if (type == '3') {
-				html+='<div style="font-size: 14px;"><b>'+entry['企业名称']+'</b></div>'; 
-				html+='<hr size="1" color="#AAA"   align="center" />';
-	            html+='<div>地址：'+entry['企业地址']+'</div>';   
-				star += '<span id="star"></span><font style="float:right">共有' + entry['评分人数'] + '个评分</font><script type="text/javascript">$(function(){$("#star").raty({readOnly:	true,size:      24,'
-		+ 'starHalf:  "star-half-big.png",'
-		+ 'starOff:   "star-off-big.png",'
-		+ 'starOn:    "star-on-big.png", start: '+entry['评分']+'	});});</script>'; 
-			}
+				+ 'starHalf:  "star-half-big.png",'
+				+ 'starOff:   "star-off-big.png",'
+				+ 'starOn:    "star-on-big.png", start: '+entry['评分']+'	});});</script>'; 
+			
 			$('#lng').html(entry['经度']);
 			$('#lat').html(entry['纬度']);
 			lng=entry['经度'];
