309b241a4efde00e1ead4a3b580332ab6018de74
javawebzwp@gmail.com <javawebzwp@gmail.com@75b40f71-ecbc-670a-4c62-a22b064bb89e>
2012-11-17 03:54:55 +0000

    改动apply到iphone

--- trunk/iphone/UcarV1.js
+++ trunk/iphone/UcarV1.js
@@ -194,3 +194,17 @@ function readfile() {
     }
     
 }
+
+function request(strName) { 
+    var strHref = window.location.href; 
+    var intPos = strHref.indexOf("?"); 
+    var strRight = strHref.substr(intPos + 1); 
+
+    var arrTmp = strRight.split("&"); 
+    for(var i = 0; i < arrTmp.length; i++) { 
+        var arrTemp = arrTmp[i].split("="); 
+
+        if(arrTemp[0].toUpperCase() == strName.toUpperCase()) return arrTemp[1]; 
+    } 
+    return ""; 
+} 
