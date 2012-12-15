02e9a183e22a4f2ac551346be6f24e0b8985aa65
ecc008lty@gmail.com <ecc008lty@gmail.com@75b40f71-ecbc-670a-4c62-a22b064bb89e>
2012-12-15 17:08:52 +0000

    更新了特殊字符处理代码。 修正分享页面检查登录的bug

--- trunk/PHP/postcomment.php
+++ trunk/PHP/postcomment.php
@@ -1,30 +1,29 @@
 <?php
 
-ini_set('default_charset','gbk');
 $GetFiltReq='select|insert|update|delete|union|into|load_file|outfile' ;
 $Err = 'Input Blocked.';
 //注释项暂时无法通过SAE检查
 
+$con = mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS) or die('Could not connect: ' . mysql_error());
+mysql_query("SET NAMES 'utf8'");
+mysql_query("SET CHARACTER_SET_CLIENT=utf8");
+mysql_query("SET CHARACTER_SET_RESULTS=utf8");
+
 
 foreach ($_GET as $GET_key=>$GET_var){
-	if (is_numeric($GET_var)) {
+ 
+ 	if (is_numeric($GET_var)) {
  	  $_GET["$GET_key"]=intval($GET_var);   //禁用浮点
 	 } else {
-  	 //if (!get_magic_quotes_gpc()) {$GET_var=addslashes($GET_var);	}	// 单双引号反斜杠转义
-  	 $GET_var = str_replace("_", "\_", $GET_var);    // 把 '_'过滤掉  
-  	 $GET_var = str_replace("%", "\%", $GET_var);    // 把 '%'过滤掉
-   	$GET_var = str_replace("<", "\<", $GET_var);    // 把 '<'过滤掉
-  	 $GET_var = str_replace(">", "\>", $GET_var);    // 把 '>'过滤掉
-   	$GET_var = str_replace(";", "\;", $GET_var);    // 把 ';'过滤掉
-   	//$GET_var = nl2br($GET_var);    		    // 回车转换 
-   	//$GET_var = htmlspecialchars($GET_var);    	   // html标记转换  
-  	 if(preg_match("/".$GetFiltReq."/i", $GET_var)==1){   //禁用一些SQL操作符
-    	$GET_var = $Err;
-   	}
+          $GET_var = "'" . mysql_real_escape_string($GET_var). "'";		// 特殊字符转义
+  		 if(preg_match("/".$GetFiltReq."/i", $GET_var)==1){   //禁用一些SQL操作敏感字
+    			$GET_var = $Err;
+   		}
    	$_GET["$GET_key"]=$GET_var;
         
  	} 
 }
+
 
 
 $shopid=$_GET["shopid"];
