9400fc6cf008a0ff6457aa166dc31c6cdfe3302d
wangyichao0122@gmail.com <wangyichao0122@gmail.com@75b40f71-ecbc-670a-4c62-a22b064bb89e>
2012-12-23 14:17:26 +0000

    商户页面基本修改完成

--- trunk/js/getcomment.js
+++ trunk/js/getcomment.js
@@ -6,11 +6,14 @@ $(document).ready(function(){
 	$.getJSON('http://ihavecar.sinaapp.com/usercomment.php?shop_id='+shopid+'&type='+type+'&jasoncallback=?&randomID='+Math.random(), function(json){
 		//random函数用来取消缓存影响,jsonp形式进行跨域传数据
 		$.each(json,function(entryIndex,entry){
-			if(count<5){
-				html+='<tr><td><img style="padding: 10px;" src="'+entry['image']+'" width="60" height="60"></td>';
-	            html+='<td><b>'+entry['nickname'] + '</b>&nbsp;&nbsp;&nbsp;&nbsp;'; 
-				html+='<span id="star'+entry['commentid']+'"></span><script type="text/javascript">$(function(){$("#star'+entry['commentid']+'").raty({readOnly:	true,start:		'+entry['score']+'	});});</script>'; 
-	            html+='<br><p>'+entry['content']+'</p>'; 
+			if(count<1){
+				html+='<tr><td><img style="font-size: 18px;font-family:黑体;padding: 0 0 0 5px;margin-top:-350px;" src="'+entry['image']+'" width="45" height="45"></td>';
+	            html+='<td><br><b>'+entry['nickname'] + '</b>&nbsp;&nbsp;&nbsp;&nbsp;';
+				html+='<span ><a href="allcomment.html?shopid='+shopid+'&type='+type+'" id="more" rel="external" align="right" style="margin-left:80px">更多</a>';
+				html+='<br><span id="star'+entry['commentid']+'"></span><script type="text/javascript">$(function(){$("#star'+entry['commentid']+'").raty({readOnly:	true,start:		'+entry['score']+'	});});</script>'; 
+				time=entry['time'].split(" ");
+				html+='<br><b>'+time[0]+'</b>'; 
+	            html+='<br><p sytle="style="font-size: 18px;font-family:黑体;">'+entry['content']+'</p>'; 
 				//html+=''+entry['time']+'  </td>';    
 	            html+='</tr>';}
 				count++;
@@ -29,15 +32,17 @@ $(document).ready(function(){
 	$.getJSON('http://ihavecar.sinaapp.com/usercomment.php?shop_id='+shopid+'&type='+type+'&jasoncallback=?&randomID='+Math.random(), function(json){
 		//random函数用来取消缓存影响,jsonp形式进行跨域传数据
 		$.each(json,function(entryIndex,entry){
-				html+='<tr><td><img style="padding: 10px;" src="'+entry['image']+'" width="60" height="60"></td>';
-	            html+='<td><b>'+entry['nickname'] + '</b>&nbsp;&nbsp;&nbsp;&nbsp;'; 
-				html+='<span id="star'+entry['commentid']+'"></span><script type="text/javascript">$(function(){$("#star'+entry['commentid']+'").raty({readOnly:	true,start:		'+entry['score']+'	});});</script>'; 
-	            html+='<br><p>'+entry['content']+'</p>'; 
+				html+='<tr><td><img style="font-size: 18px;font-family:黑体;padding: 0 0 0 5px;margin-top:-350px;" src="'+entry['image']+'" width="45" height="45"></td>';
+	            html+='<td><br><b>'+entry['nickname'] + '</b>&nbsp;&nbsp;&nbsp;&nbsp;';
+				html+='<br><span id="star'+entry['commentid']+'"></span><script type="text/javascript">$(function(){$("#star'+entry['commentid']+'").raty({readOnly:	true,start:		'+entry['score']+'	});});</script>'; 
+				time=entry['time'].split(" ");
+				html+='<br><b>'+time[0]+'</b>'; 
+	            html+='<br><p sytle="style="font-size: 18px;font-family:黑体;">'+entry['content']+'</p>'; 
 				//html+=''+entry['time']+'  </td>';    
 	            html+='</tr>';
 	    });
 		html+='</table>';
-	    $('#comment').html(html); 
+	    $('#allcomment').html(html); 
 	}); 
 });  
 }
--- trunk/js/getshopinfo.js
+++ trunk/js/getshopinfo.js
@@ -10,9 +10,9 @@ $(document).ready(function(){
 				shopname=entry['企业名称'];
 	      html+='<div style="font-size: 24px; "><b>'+entry['企业名称']+'</b></div><br>'; 
 				//html+='<div id="shopdetails"><div><br><a href="tel:' + entry['联系电话'] + '" style="font-size: 18px; padding: 10px 0 0 10px;">电话：'+entry['联系电话']+'</a></div>';
-				html+='<div id="shopdetails"><div style="font-size: 18px;font-family:黑体; padding: 10px 4px 20px 10px;">电话：'+entry['联系电话']+'<a href="tel:' + entry['联系电话'] + '"><img src="img/phone.png" align="right" style="padding: 0 0 0 3px;margin-right:10px;"></a></div>';
+				html+='<div id="shopdetails"><div style="font-size: 18px;font-family:黑体; padding: 10px 4px 15px 10px;">电话：'+entry['联系电话']+'<a href="tel:' + entry['联系电话'] + '"><img src="img/phone.png" align="right" style="padding: 0 0 0 3px;margin-right:10px;"></a></div>';
 				html+='<hr size="1" color="#AAA"   align="center" />';
-	      html+='<div style="font-size: 18px;font-family:黑体; padding: 0 10px 30px 10px;">地址：'+entry['企业地址']+'<img src="img/location.png" align="right" style="padding: 0 0 3px 3px;margin-right:4px;"></div></div>';   
+	      html+='<div style="font-size: 18px;font-family:黑体; padding: 0 10px 20px 10px;">地址：'+entry['企业地址']+'<img src="img/location.png" align="right" style="padding: 0 3px 3px 3px;margin-right:4px;"></div></div>';   
 				star += '<span id="star"></span><script type="text/javascript">$(function(){$("#star").raty({readOnly:	true,size:      24,'
 				+ 'starHalf:  "star-half-big.png",'
 				+ 'starOff:   "star-off-big.png",'
